ebd3ac33d716ca146afc43e0ac0782df8b1c769a
Yash217438 <yashraj2605@gmail.com>
2026-06-11 13:14:31 +0530

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="NGT Dashboard",
+    layout="wide"
+)
+
+st.title("NGT Sewage Management Dashboard")
+
+st.markdown("""
+### Available Dashboards
+
+- ULB 2023
+- ULB 2025
+- ULB 2026
+- STP 2023
+- STP 2025
+- STP 2026
+""")
